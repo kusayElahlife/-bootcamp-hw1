495f2667cc1e179692e8872a06682a5d4b1a234a
Kusay Elhalife <74819899+kusayElahlife@users.noreply.github.com>
2020-12-20 21:01:33 +0300

    Initial commit

---
+++ src/libs/hw1/components/my-card/my-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-card',
+  templateUrl: './my-card.component.html',
+  styleUrls: ['./my-card.component.scss']
+})
+export class MyCardComponent implements OnInit {
+
+  // TODO: define @Input(s) here
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/libs/hw1/containers/dashboard-container/dashboard-container.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-dashboard-container',
+  templateUrl: './dashboard-container.component.html',
+  styleUrls: ['./dashboard-container.component.scss']
+})
+export class DashboardContainerComponent implements OnInit {
+  cards: { title: string, body: string }[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    //Loading posts from jsonplaceholder.typicode
+    this.http.get("https://jsonplaceholder.typicode.com/comments").subscribe((comments: any) => {
+      console.log(comments) // See incoming data in the browser console and remove this log
+      // TODO: set cards array with valid objects
+      // this.cards = comments. ... map? reduce? filter?
+    })
+  }
+
+}
